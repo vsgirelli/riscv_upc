0c60152d53ff9d40a2c1446ef5b6b2b93c45def3
vsgirelli <vavi_sg@hotmail.com>
2024-01-05 15:56:39 +0100

    Adjusting multiplier input/output to new instruction decoded type

--- src/pipelined_multiplier.sv
+++ src/pipelined_multiplier.sv
@@ -1,28 +1,29 @@
 // Artificial 5-stage multiplier
-module pipelined_multiplier #(parameter WIDTH = 32)(
-  input wire clk,
-  input wire rst,
-  input wire [WIDTH-1:0] operand1,
-  input wire [WIDTH-1:0] operand2,
-  output reg [2*WIDTH-1:0] mult_result // The output is 64-bit and it should match on the calling modules as well
+module pipelined_multiplier (
+  input logic clk,
+  input logic rst,
+  input inst_decoded_t inst_mul_in,
+  output inst_decoded_t inst_mul_out
 );
 
-reg [2*WIDTH-1:0] stage1, stage2, stage3, stage4, stage5;
+reg [2*ARCH_LEN-1:0] stage1, stage2, stage3, stage4, stage5;
 
 always @(posedge clk or negedge rst) begin
   if (!rst) begin
     // Reset all pipeline stages
-    stage1 <= 2*{WIDTH{1'b0}};
-    stage2 <= 2*{WIDTH{1'b0}};
-    stage3 <= 2*{WIDTH{1'b0}};
-    stage4 <= 2*{WIDTH{1'b0}};
-    stage5 <= 2*{WIDTH{1'b0}};
+    stage1 <= 2*{ARCH_LEN{1'b0}};
+    stage2 <= 2*{ARCH_LEN{1'b0}};
+    stage3 <= 2*{ARCH_LEN{1'b0}};
+    stage4 <= 2*{ARCH_LEN{1'b0}};
+    stage5 <= 2*{ARCH_LEN{1'b0}};
   end else begin
-    stage1 = operand1 * operand2;
+    stage1 = inst_mul_in.src_data_1 * inst_mul_in.src_data_2;
     stage2 = stage1;
     stage3 = stage2;
     stage4 = stage3;
-    mult_result = stage4;
+    inst_mul_out = inst_mul_in;
+    inst_mul_out.dst_reg_data = stage4;
+    inst_mul_out.reg_data_ready = 1;
   end
 end
 
