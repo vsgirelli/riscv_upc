f65a9fbb7fdfec1f4dabaabf7377b766351836a9
Marc Clascà <marc.clasca@bsc.es>
2024-01-15 19:15:11 +0100

    Adds integer computation to ALU

--- src/execute_stage.sv
+++ src/execute_stage.sv
@@ -9,11 +9,25 @@ module execute_stage (
 );
 
 assign inst_exe_out = inst_exe_in;
-/*alu alu_inst (
-  .operand1(operand1),
-  .operand2(operand2),
-  .alu_op(alu_op),
+
+logic [ARCH_LEN-1:0] op1, op2;
+
+always_comb begin
+    op1 <= inst_exe_in.src_data_2;
+
+    if(inst_exe_in.is_i) op2 <= inst_exe_in.immediate;
+    else op2 <= inst_exe_in.src_data_2;
+end
+
+
+
+alu alu_inst (
+  .operand1(op1),
+  .operand2(op2),
+  .is_i(inst_exe_in.is_i
+  .alu_op3(inst_exe_in.func3),
+  .alu_op7(inst_exe_in.func7),
   .alu_result(alu_result)
-);*/
+);
 
 endmodule
