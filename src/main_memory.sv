f0e6e5f7d67bc605026a193e2c0f3c7e43f44790
vsgirelli <vavi_sg@hotmail.com>
2024-01-22 00:02:37 +0100

    Adding test instructions that generate the need for bypasses and bubble insertion

--- src/main_memory.sv
+++ src/main_memory.sv
@@ -8,8 +8,17 @@ module main_memory (
 );
 
 typedef enum {IDLE, SERVING} tb_state_t;
-tb_state_t state, nextState;
-const logic [ICLLEN-1:0] line = 128'h00408093_00308093_00208093_00108093;
+tb_state_t state, nextState;      //     sw      add       lw      addi
+const logic [ICLLEN-1:0] line   = 128'h001080A3_003100B3_00108183_00108093;
+//const logic [ICLLEN-1:0] line = 128'h00408093_001080A3_001100B3_00108093;
+//       imm        rs1   f3   rt    op  addi
+//32'b000000000001_00001_000_00001_0010011
+//       imm        rs1   f3   rt    op  lw 
+//32'b000000000001_00001_000_00011_0000011
+//    f3      rs2    rs1   f3  rt     op  add
+//32'b0000000_00011_00010_000_00001_0110011
+//    im      rs2    rs1   f3  im     op   sw
+//32'b0000000_00001_00001_000_00001_0100011
 
 
 always_comb begin
