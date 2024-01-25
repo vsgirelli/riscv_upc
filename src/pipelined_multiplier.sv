d2424f7426c7d1ba66645ee27542e7587318b433
Marc Clascà <marc.clasca@bsc.es>
2024-01-25 03:39:59 +0100

    Pipelined multiplier finished not tested

--- src/pipelined_multiplier.sv
+++ src/pipelined_multiplier.sv
@@ -6,15 +6,16 @@ module pipelined_multiplier (
   input logic clk,
   input logic rst,
   input inst_decoded_t inst_mul_in,
-  output inst_decoded_t inst_mul_out
-  input logic kill_mul,
-  input lofic stall_mul_in,
+  output inst_decoded_t inst_mul_out,
+  input logic stall_mul_in,
   output logic stall_mul_out
 );
 
 inst_decoded_t instMS_ff[3:0];
 inst_decoded_t nxInstMS_ff[3:0];
                 
+logic [2*ARCH_LEN-1:0] mult_result;
+
 genvar i;
 generate
 
@@ -26,7 +27,7 @@ for(i = 1; i < 4; i++) begin : ms_ff_cotrol
 
     // TODO: convert killMS and analogous to kill (from the graduation
     //       list)
-    if (rst | kill_mul) nxInstMS_ff[i].valid <= 0;
+    if (rst) nxInstMS_ff[i].valid <= 0;
   end
 
   always_ff @(posedge clk)
@@ -36,7 +37,13 @@ endgenerate
 
 always_comb begin
     nxtInstMS_ff[0] = inst_mul_in;
-    nxtInstMS_ff[0].valid = inst_mul_in.is_m;
+    nxtInstMS_ff[0].valid = inst_mul_in.is_m & inst_mul_in.valid;
     
+    mult_result = instMS_ff[3].src_data_1 * instMS_ff[3].src_data_2;
+    inst_mul_out = instMS_ff[3];
+    inst_mul_out.dst_reg_data = mult_result[ARCH_LEN-1:0];
+    inst_mul_out.reg_data_ready = inst_MS_ff[3];
+
+    stall_mul_out = instMS_ff[0].valid | instMS_ff[1].valid | instMS_ff[2].valid | instMS_ff[3].valid;
 
 endmodule
